48bf146d60ad6a2ec01997e62bbfbcfe87bdbbf9
FreskyZ <FreskyZ@outlook.com>
2026-09-22 19:13:50 +0800

    recipe: update data structure
    that should be more suitable for manual editing

--- recipe/make-page.ts
+++ recipe/make-page.ts
@@ -5,21 +5,292 @@ import yaml from 'yaml';
 
 interface ItemData {
     name: string,
-    kind?: 'seed' | 'liquid' | 'bottle' | 'filled',
+    kind?: 'seed' | 'liquid' | 'bottle' | 'gas' | 'filled' | 'bottle+liquid' | 'bottle+gas',
     icon: string,
     desc: string,
 }
 interface RecipeData {
+    name: string,
     machine: string,
+    time: number,
+    vibe?: string,
     inputs: { name: string, count: number }[],
     outputs: { name: string, count: number }[],
-    time: number,
-    name: string,
-    kind?: string,
+}
+interface DataConfig {
+    plants: string[],
+    bottles: string[],
+    liquids: string[],
+    gasitems: string[],
+    minerals: string[],
+    machines: string[],
+    environments: string[],
 }
 
-// const items = JSON.parse(await fs.readFile('recipe/item.json', 'utf-8')) as ItemData[];
-// const recipes = JSON.parse(await fs.readFile('recipe/recipe.json', 'utf-8')) as RecipeData[];
+async function processData(filepath: string) {
+
+    const alldata: {
+        配方: Record<string, string>,
+        配置: Record<string, string[]>,
+    } = yaml.parse(await fs.readFile(filepath, 'utf-8'));
+    const dataconfig: DataConfig = {
+        plants: alldata.配置.植物,
+        bottles: alldata.配置.瓶子,
+        liquids: alldata.配置.液体,
+        gasitems: alldata.配置.气体,
+        minerals: alldata.配置.矿物,
+        machines: alldata.配置.机器,
+        environments: alldata.配置.环境,
+    };
+
+    let hasError = false;
+    const recipes: RecipeData[] = [];
+    for (const [name, raw] of Object.entries(alldata.配方)) {
+        const splitted1 = raw.split('=>[').map(x => x.trim());
+        if (splitted1.length != 2) {
+            hasError = true;
+            console.log(`${name}: invalid format, expect one =>[`);
+            continue;
+        }
+        const [rawInput, rawConditionAndOutputs] = splitted1;
+        const splitted2 = rawConditionAndOutputs.split(']=>').map(x => x.trim());
+        if (splitted2.length != 2) {
+            hasError = true;
+            console.log(`${name}: invalid format, expect one ]=>`);
+            continue;
+        }
+        const [rawCondition, rawOutput] = splitted2;
+
+        const inputs = rawInput.split('+').map(x => x.trim()).map(rawInput => {
+            const match = /^(\d+)/.exec(rawInput);
+            if (match && match[1] == '1') {
+                console.log(`${name}: input ${rawInput} no need to add 1`);
+            }
+            return match 
+                ? { count: +match[1], name: rawInput.substring(match[1].length) }
+                : { count: 1, name: rawInput };
+        });
+        const outputs = rawOutput == '无'
+            ? []
+            : rawOutput.split('+').map(x => x.trim()).map(rawOutput => {
+                const match = /^(\d+)/.exec(rawOutput);
+                if (match && match[1] == '1') {
+                    console.log(`${name}: output ${rawOutput} no need to add 1`);
+                }
+                return match 
+                    ? { count: +match[1], name: rawOutput.substring(match[1].length) }
+                    : { count: 1, name: rawOutput };
+            });
+        const conditions = rawCondition.split('+').map(x => x.trim());
+        let machine: string;
+        let time = 2;
+        let vibe: string = undefined;
+        for (const condition of conditions) {
+            if (dataconfig.machines.includes(condition)) {
+                machine = condition;
+            } else if (condition.endsWith('环境')) {
+                const environment = condition.substring(0, condition.length - 2);
+                if (dataconfig.environments.includes(environment)) {
+                    vibe = environment;
+                } else {
+                    hasError = true;
+                    console.log(`${name}: unknown environment`);
+                }
+            } else if (condition.endsWith('s')) {
+                time = +condition.substring(0, condition.length - 1);
+                if (isNaN(time)) {
+                    hasError = true;
+                    console.log(`${name}: unknown time`);
+                } else if (time == 2) {
+                    console.log(`${name}: no need to write explicit 2s`);
+                }
+            } else {
+                hasError = true;
+                console.log(`${name}: unknown condition ${condition}`);
+            }
+        }
+        recipes.push({ name, time, machine, vibe, inputs, outputs });
+    }
+    if (hasError) { return null; }
+
+    const allItemNames: Set<String> = new Set();
+    const usedPlants: Set<string> = new Set();
+    const usedMinerals: Set<string> = new Set();
+    for (const recipe of recipes) {
+        recipe.inputs.forEach(i => allItemNames.add(i.name));
+        recipe.outputs.forEach(o => allItemNames.add(o.name));
+
+        if (recipe.machine == '种植机' || recipe.machine == '采种机') {
+            console.log(`${recipe.name}: don't add obvious plant recipes`);
+            continue;
+        }
+        if (recipe.inputs.length == 2 && (
+            dataconfig.bottles.includes(recipe.inputs[0].name) && dataconfig.liquids.includes(recipe.inputs[1].name)
+            || dataconfig.liquids.includes(recipe.inputs[0].name) && dataconfig.bottles.includes(recipe.inputs[1].name)
+            || dataconfig.bottles.includes(recipe.inputs[0].name) && dataconfig.gasitems.includes(recipe.inputs[1].name)
+            || dataconfig.gasitems.includes(recipe.inputs[0].name) && dataconfig.bottles.includes(recipe.inputs[1].name)
+        )) {
+            console.log(`${recipe.name}: don't add fill bottle recipes`);
+        }
+        if (recipe.outputs.length == 2 && (
+            dataconfig.bottles.includes(recipe.outputs[0].name) && dataconfig.liquids.includes(recipe.outputs[1].name)
+            || dataconfig.liquids.includes(recipe.outputs[0].name) && dataconfig.bottles.includes(recipe.outputs[1].name)
+            || dataconfig.bottles.includes(recipe.outputs[0].name) && dataconfig.gasitems.includes(recipe.outputs[1].name)
+            || dataconfig.gasitems.includes(recipe.outputs[0].name) && dataconfig.bottles.includes(recipe.outputs[1].name)
+        )) {
+            console.log(`${recipe.name}: don't add pour bottle recipes`);
+        }
+
+        let inputIsSystemInputCount = 0;
+        for (const input of recipe.inputs) {
+            if (dataconfig.plants.includes(input.name)) {
+                inputIsSystemInputCount += 1;
+                usedPlants.add(input.name);
+            } else if (dataconfig.minerals.includes(input.name)) {
+                inputIsSystemInputCount += 1;
+                usedMinerals.add(input.name);
+            } else if (recipes.some(r => r.outputs.some(o => o.name == input.name))) {
+                // normal item that produced by other recipes
+            } else {
+                console.log(`${recipe.name}: input item ${input.name} not found in plants, minerals or other recipe outputs`);
+            }
+        }
+        if (inputIsSystemInputCount == recipe.inputs.length
+            && recipe.outputs.length == 1 && !recipes.some(r => r.inputs.some(i => i.name == recipe.outputs[0].name))
+        ) {
+            console.log(`${recipe.name}: consider reject irrelavent simple recipes?`);
+        }
+    }
+
+    const unusedPlants = dataconfig.plants.filter(p => !usedPlants.has(p));
+    if (unusedPlants.length) {
+        console.log(`plants unused: ${unusedPlants.join(', ')}`);
+    }
+    const unusedMinerals = dataconfig.minerals.filter(p => !usedMinerals.has(p));
+    if (unusedMinerals.length) {
+        console.log(`minerals unused: ${unusedMinerals.join(', ')}`);
+    }
+    const unusedConfigItems = dataconfig.bottles
+        .concat(dataconfig.liquids).concat(dataconfig.gasitems).filter(p => !allItemNames.has(p));
+    if (unusedConfigItems.length) {
+        console.log(`config items unused: ${unusedConfigItems.join(',')}`);
+    }
+
+    const resultdata = {
+        items: [], // TODO coordinates
+        recipes: recipes.map(r => ({
+            name: r.name,
+            machine: r.machine,
+            time: r.time == 2 ? undefined : r.time,
+            vibe: r.vibe,
+            inputs: r.inputs.map(i => ({
+                name: i.name,
+                count: i.count == 1 ? undefined : i.count,
+            })),
+            outputs: r.outputs.map(o => ({
+                name: o.name,
+                count: o.count == 1 ? undefined : o.count,
+            })),
+        })),
+    };
+    let sb = '';
+    sb += '{"items":[\n';
+    sb += '],"recipes":[\n  ';
+    sb += resultdata.recipes.map(r => JSON.stringify(r)).join(',\n  ');
+    sb += '\n]}';
+    await fs.writeFile('recipe/data-new.json', sb);
+}
+await processData('recipe/data.yml');
+
+// TODO in result json, default time to 2, default count to 1
+
+
+// recipes.sort((r1, r2) => Buffer.from(r1.name).compare(Buffer.from(r2.name)));
+
+// let sb = '';
+// sb += '配方:\n'
+// for (const recipe of recipes) {
+//     sb += '  ' + recipe.name + ': ';
+//     for (const input of recipe.inputs) {
+//         if (input.count > 1) { sb += `${input.count}`; }
+//         sb += input.name;
+//         sb += ' + ';
+//     }
+//     sb = sb.substring(0, sb.length - 3);
+//     sb += ` ->[${recipe.machine},${recipe.time}s]-> `;
+//     for (const output of recipe.outputs) {
+//         if (output.count > 1) { sb += `${output.count}`; }
+//         sb += output.name;
+//         sb += ' + ';
+//     }
+//     if (recipe.outputs.length) {
+//         sb = sb.substring(0, sb.length - 3);
+//     } else {
+//         sb += '无';
+//     }
+//     sb += '\n';
+// }
+// sb += '\n'
+// sb += '物品类别:\n';
+// sb += '  种子: [' + items.filter(i => i.kind == 'seed').map(i => i.name).join(', ') + ']\n';
+// sb += '  瓶子: [' + items.filter(i => i.kind == 'bottle').map(i => i.name).join(', ') + ']\n';
+// sb += '  液体: [' + items.filter(i => i.kind == 'liquid').map(i => i.name).join(', ') + ']\n';
+// sb += '  气体: [' + items.filter(i => i.kind == 'gas').map(i => i.name).join(', ') + ']\n';
+// sb += '  瓶子+液体: [' + items.filter(i => i.kind == 'filled').map(i => i.name).join(', ') + ']\n';
+// sb += '  瓶子+气体: [' + items.filter(i => i.kind == 'bottle+gas').map(i => i.name).join(', ') + ']\n';
+// await fs.writeFile('recipe/data-order.yml', sb);
+
+// try align recipes
+// RESULT: no, vscode + cascadia code default settings don't align cjk characters
+// const formattedRecipes: string[][] = [];
+// for (const recipe of recipes) {
+//     const part1 = `  ${recipe.name}: `;
+//     let part2 = '';
+//     for (const input of recipe.inputs) {
+//         if (input.count > 1) { part2 += `${input.count}`; }
+//         part2 += input.name;
+//         part2 += ' + ';
+//     }
+//     part2 = part2.substring(0, part2.length - 3);
+//     const part3 = ` =>[${recipe.machine}`;
+//     let part4 = '';
+//     if (recipe.time != 2) {
+//         part4 += `+${recipe.time}s`;
+//     }
+//     part4 += `]=> `;
+//     let part5 = '';
+//     for (const output of recipe.outputs) {
+//         if (output.count > 1) { part5 += `${output.count}`; }
+//         part5 += output.name;
+//         part5 += ' + ';
+//     }
+//     if (recipe.outputs.length) {
+//         part5 = part5.substring(0, part5.length - 3);
+//     } else {
+//         part5 += '无';
+//     }
+//     formattedRecipes.push([part1, part2, part3, part4, part5]);
+// }
+// // make the => between part 2 and 3 align, that is, respect longest part 1 + 2
+// // whitespace is added between 1 and 2, so 1 and 2 is not in one part
+// const longestPart1And2 = formattedRecipes.reduce((v, r) => Math.max(v, r[0].length + r[1].length), 0);
+// // make the => between part 4 and 5 align, that is, respect longest part 3 + 4
+// // whitespace is added between 3 and 4, so 3 and 4 is not in one part
+// const longestPart3And4 = formattedRecipes.reduce((v, r) => Math.max(v, r[2].length + r[3].length), 0);
+
+// let sb = '';
+// sb += '配方:\n'
+// for (const recipe of formattedRecipes) {
+//     sb += recipe[0];
+//     sb += new Array(longestPart1And2 - recipe[0].length - recipe[1].length).fill(' ').join('');
+//     sb += recipe[1] + recipe[2];
+//     sb += new Array(longestPart3And4 - recipe[2].length - recipe[3].length).fill(' ').join('');
+//     sb += recipe[3] + recipe[4];
+//     sb += '\n';
+// }
+// await fs.writeFile('recipe/data-align.yml', sb);
+
+
 
 // // not include bottle + liquid recipes
 // for (const recipe of recipes) {
